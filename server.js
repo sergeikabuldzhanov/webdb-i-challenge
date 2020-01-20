0077ec6641b7a435ec3b0d9ba6f6aa4451e98c44
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-20 18:54:36 +0500

    Self study and helpers

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
+//DB helpers
+function get(id) {
+  if (id) {
+    return db("accounts").where({ id });
+  }
+  return db("accounts");
+}
+
+function insert(data) {
+    return db('accounts').insert(data);
+}
+
+function update(id, data) {
+    return db('accounts').where({id}).update(data);
+}
+
+function remove(id) {
+    return db('accounts').where({id}).del();
+}
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 
 module.exports = server;
