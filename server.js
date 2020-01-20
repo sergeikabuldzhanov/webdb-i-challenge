e1a0f45b35b356693335a4eb0ac107450d18775d
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-20 19:19:04 +0500

    Server error handler, cors, helmet

--- server.js
+++ server.js
@@ -2,31 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const db = require("./data/dbConfig.js");
-//DB helpers
-function get(id) {
-  if (id) {
-    return db("accounts").where({ id });
-  }
-  return db("accounts");
-}
-
-function insert(data) {
-    return db('accounts').insert(data);
-}
-
-function update(id, data) {
-    return db('accounts').where({id}).update(data);
-}
-
-function remove(id) {
-    return db('accounts').where({id}).del();
-}
+//routers
+const accountsRouter = require("./accounts-router");
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+server.use("/api/accounts", accountsRouter);
+
+function serverErrorCatcher(error,req,res,next) {
+    res.status(500).json(error);
+}
+
+server.use(serverErrorCatcher);
 
 module.exports = server;
