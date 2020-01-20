26076f79972deeb4c1bdc261b38378ef8c6a7ed7
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-20 19:18:43 +0500

    Created account router for .../api/accounts

---
+++ accounts-router.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { get, insert, remove, update } = require("./accounts-model");
+
+const router = express.Router();
+
+//GET ROUTES
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await get();
+    res.status(200).json(accounts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const account = await get(id);
+    res.status(200).json(account);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST ROUTES
+router.post("/", async (req, res, next) => {
+  const { name, budget } = req.body;
+  try {
+    const posted = await insert({ name, budget });
+    res.status(201).json(posted);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//PUT ROUTES
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { budget, name } = req.body;
+  try {
+    const updated = await update(id, { budget, name });
+    res.status(200).json(updated);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE ROUTES
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deleted = await remove(id);
+    res.status(200).json(deleted);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
